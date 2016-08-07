53edad0939c5e5df78b25afbbf66132d1182c62c
huaixiaoz <hello@itmp.top>
2016-08-07 21:35:45 +0800

    ..

--- FKPersonTest.m
+++ FKPersonTest.m
@@ -6,5 +6,10 @@ int main(int argc, char* argv[]){
     person = [[FKPerson alloc] init];
     
     [person say:@"hello, I love iOS"];
+    [person setName: @"孙悟空" andAge: 500];
+    NSString* info = [person info];
+    NSLog(@"person 的info信息为: %@", info);
+    [FKPerson foo];
+    
 }
 
