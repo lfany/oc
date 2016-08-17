cf3f924d2896979626ffc89cdabce7db2f9aa036
leehz <hello@itmp.top>
2016-08-17 12:35:28 +0800

    FKDog

---
+++ 160817/FKDog.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+@interface FKDog : NSObject
+{
+	@package
+	NSString* name;
+	NSString* _name;
+}
+@end
---
+++ 160817/FKDog.m
@@ -0,0 +1,18 @@
+#import "FKDog.h"
+
+@implementation FKDog
+{
+	int age;
+}
+@end
+
+int main(int argc, char* argv[]){
+	@autoreleasepool{
+		FKDog* dog = [[FKDog alloc]init];
+		[dog setValue: @"旺财" forKey: @"name"];
+		NSLog(@"dog->name: %@", dog->name);
+		NSLog(@"dog->_name: %@", dog->_name);
+		[dog setValue: [NSNumber numberWithInt: 5 forKey: @"age"]];
+		NSLog(@"dog->age: %@", [dog valueForKey: @"age"]);
+	}
+}
